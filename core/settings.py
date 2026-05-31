9a0db1f93d2ff89553290a8c8c45c4575eed7710
Ryionya <jrsumiran@gmail.com>
2026-05-31 22:42:31 +0800

    Added a forgot password option

--- core/settings.py
+++ core/settings.py
@@ -218,6 +218,15 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'BloodLink Support <rhinesumiran@gmail.com>'
+
 WEBPUSH_SETTINGS = {
     'VAPID_PUBLIC_KEY': os.getenv('VAPID_PUBLIC_KEY'),
     'VAPID_PRIVATE_KEY': os.getenv('VAPID_PRIVATE_KEY'),
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/',      admin.site.urls),
@@ -12,4 +14,32 @@ urlpatterns = [
     path('webpush/',    include('webpush.urls')),        # push notifications
     path('',            include('pwa.urls')),            # PWA manifest / sw
     path('dashboard/',  include('dashboard.urls')),      # admin dashboard
+    
+    # Password reset
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html',
+             email_template_name='accounts/password_reset_email.html',
+             subject_template_name='accounts/password_reset_subject.txt',
+         ), 
+         name='password_reset'),
+    
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'
+         ), 
+         name='password_reset_done'),
+    
+    path('password-reset/confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html'
+         ), 
+         name='password_reset_confirm'),
+    
+    path('password-reset/complete/', 
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ), 
+         name='password_reset_complete'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
